d698be882fefec4f0de693df06c604ce1e2ccb21
Mimoja <git@mimoja.de>
2023-03-10 19:45:19 +0100

    Implement get
    Signed-off-by: Mimoja <git@mimoja.de>

--- src/varstore.rs
+++ src/varstore.rs
@@ -51,6 +51,22 @@ impl Varstore {
             None => self.variables.iter().next(),
         }
     }
+
+    fn request_get(&self, name: &String) -> Option<&EfiVariable> {
+        if name.is_empty() {
+            return None;
+        }
+
+        let var = self.variables.get(name);
+
+        if var.is_some()
+            && self.bootservices_exited
+            && !var.unwrap().attr.contains(EfiAttribute::RUNTIME_ACCESS)
+        {
+            return None;
+        }
+        var
+    }
 }
 
 #[cfg(test)]
@@ -98,5 +114,66 @@ mod tests {
 
         let non_exist = varstore.request_get_next(Some("Test_NonExist".to_string()));
         assert!(non_exist.is_none());
+    }
+
+    #[test]
+    fn get_full_access() {
+        let mut varstore = Varstore::new();
+        let var = EfiVariable::new(
+            vec![1, 2, 3, 4],
+            EfiAttribute::BOOTSERVICE_ACCESS | EfiAttribute::RUNTIME_ACCESS,
+        );
+        varstore
+            .variables
+            .insert("TestExist".to_string(), var.clone());
+
+        assert!(varstore.request_get(&String::from("TestExist")).is_some());
+        assert_eq!(
+            varstore
+                .request_get(&String::from("TestExist"))
+                .unwrap()
+                .data,
+            vec![1, 2, 3, 4]
+        );
+        assert!(varstore
+            .request_get(&String::from("TestNonExist"))
+            .is_none());
+    }
+
+    #[test]
+    fn get_runtime_access() {
+        let mut varstore = Varstore::new();
+
+        let var1 = EfiVariable::new(vec![1, 2, 3, 4], EfiAttribute::BOOTSERVICE_ACCESS);
+        varstore
+            .variables
+            .insert("TestBootservice".to_string(), var1.clone());
+
+        let var2 = EfiVariable::new(
+            vec![4, 3, 2, 1],
+            EfiAttribute::BOOTSERVICE_ACCESS | EfiAttribute::RUNTIME_ACCESS,
+        );
+        varstore
+            .variables
+            .insert("TestRuntime".to_string(), var2.clone());
+
+        assert!(varstore
+            .request_get(&String::from("TestBootservice"))
+            .is_some());
+
+        varstore.bootservices_exited = true;
+
+        assert!(varstore
+            .request_get(&String::from("TestBootservice"))
+            .is_none());
+
+        assert!(varstore.request_get(&String::from("TestRuntime")).is_some());
+        assert_eq!(
+            varstore
+                .request_get(&String::from("TestRuntime"))
+                .unwrap()
+                .data,
+            vec![4, 3, 2, 1]
+        );
     }
 }
